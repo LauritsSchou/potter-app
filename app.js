0cad96f26571b37916d57de728e0cdb4f83ccf73
LauritsSchou <laurits.schou@gmail.com>
2023-03-20 16:33:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,29 +16,35 @@ function initApp() {
   const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 
   showCharacter(potterImage, potterName, potterHouse, potterDateOfBirth, potterActor);
-  //   showCharacter(hermioneImage, hermioneName, hermioneHouse, hermioneDateOfBirth, hermioneActor);
   showCharacter("http://hp-api.herokuapp.com/images/ron.jpg", "Ron Weasley", "Gryffindor", "01-03-1980", "Rupert Grint");
   console.log(potterName, potterGender, potterHouse, potterDateOfBirth, potterAncestry, potterEyeColour, potterHairColour, potterActor, potterImage);
 }
-function showCharacter(image, name, house, dateOfBirth, actor) {
-  const articleElement = document.createElement("article");
-  const nameElement = document.createElement("h2");
-  const imageElement = document.createElement("img");
-  const actorElement = document.createElement("p");
-  const houseElement = document.createElement("p");
-  const dateOfBirthElement = document.createElement("p");
-  nameElement.textContent = name;
-  imageElement.src = image;
-  actorElement.textContent = actor;
-  houseElement.textContent = house;
-  dateOfBirthElement.textContent = dateOfBirth;
-  articleElement.appendChild(imageElement);
-  articleElement.appendChild(nameElement);
-  articleElement.appendChild(actorElement);
-  articleElement.appendChild(dateOfBirthElement);
-  articleElement.appendChild(houseElement);
-  document.querySelector("#characters").appendChild(articleElement);
-}
-// function showCharacter() {
-//   const myHtml = /*html*/ <article> </article>;
+// function showCharacter(image, name, house, dateOfBirth, actor) {
+//   const articleElement = document.createElement("article");
+//   const nameElement = document.createElement("h2");
+//   const imageElement = document.createElement("img");
+//   const actorElement = document.createElement("p");
+//   const houseElement = document.createElement("p");
+//   const dateOfBirthElement = document.createElement("p");
+//   nameElement.textContent = name;
+//   imageElement.src = image;
+//   actorElement.textContent = actor;
+//   houseElement.textContent = house;
+//   dateOfBirthElement.textContent = dateOfBirth;
+//   articleElement.appendChild(imageElement);
+//   articleElement.appendChild(nameElement);
+//   articleElement.appendChild(actorElement);
+//   articleElement.appendChild(houseElement);
+//   articleElement.appendChild(dateOfBirthElement);
+//   document.querySelector("#characters").appendChild(articleElement);
 // }
+function showCharacter() {
+  const myHtml = /*html*/ `<article>
+    <img src="http://hp-api.herokuapp.com/images/harry.jpg" >
+    <h2>Harry Potter</h2>
+    <p>Gryffindor</p>
+    <p>Date of Birth: 31-07-1980</p>
+    <p>Played by Daniel Radcliffe</p>
+    </article>`;
+  document.querySelector("#characters").insertAdjacentHTML("beforeend", myHtml);
+}
